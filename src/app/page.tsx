a64a2fcee22f821039262a48b1606bd99651ca59
Ahmed_Mansour <78514942+Up-to-code@users.noreply.github.com>
2024-07-12 04:12:27 +0300

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,54 @@
-import Head from "next/head";
+"use client";
+import ReactPlayer from "react-player";
 
 const Home: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
-      <Head>
-        <title>Download Our E-Commerce App</title>
-        <meta
-          name="description"
-          content="Download our e-commerce app to enjoy the best shopping experience"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="flex flex-col items-center min-h-screen py-2 bg-gray-100">
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">Welcome to Our E-Commerce App</h1>
-        <p className="mt-3 text-2xl">
-          Download our app to enjoy the best shopping experience
-        </p>
 
-        <div className="flex mt-6 space-x-4">
-          <a
-            href="https://expo.dev/artifacts/eas/abKjqWtE3xhArRooFky2Wi.apk"
-            className="px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-500"
-          >
-            Download for Android
-          </a>
+      <main className="flex flex-col-reverse lg:flex-row items-center justify-center flex-1 px-10 lg:px-20 text-center   gap-10">
+        <div className="w-full max-w-md justify-start">
+          <h1 className="text-3xl font-bold lg:text-start">
+            Welcome to Our E-Commerce App
+          </h1>
+          <p className="mt-3 text-xl lg:text-start">
+            Download our app to enjoy the best shopping experience
+          </p>
+
+          <div className="flex mt-6 space-x-4 justify-center lg:justify-start">
+            <a
+              href="https://expo.dev/artifacts/eas/abKjqWtE3xhArRooFky2Wi.apk"
+              className="px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-500"
+            >
+              Download for Android
+            </a>
+          </div>
+        </div>
+        <div>
+          <div className="mt-10 w-full max-w-4xl">
+            <div className="mt-4 aspect-w-16 aspect-h-9">
+              <ReactPlayer
+                url="https://firebasestorage.googleapis.com/v0/b/k-store-app.appspot.com/o/Advanced%20Mobile%20App%20Promo_free.mp4?alt=media&token=1e41f35d-babf-4439-91d3-a688716b1804"
+                controls
+                width={window.screen.availWidth > 600 ? 400 : 300}
+                height={300}
+                className="rounded-lg shadow-lg  overflow-hidden"
+                key={"video"}
+                light="https://firebasestorage.googleapis.com/v0/b/k-store-app.appspot.com/o/sdsad.png?alt=media&token=7ad011a6-5f0e-4db9-a757-df791b2d5d29"  // Replace with your cover image URL
+
+                config={{
+                  file: {
+                    attributes: {
+                      controlsList: "nodownload",
+                    },
+                  
+                    
+                  },
+                  
+                }}
+              />
+            </div>
+          </div>
         </div>
       </main>
 
